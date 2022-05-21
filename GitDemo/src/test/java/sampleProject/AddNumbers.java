57d0f8ca9970e9731a8ca71eb2d5cf3305b3e09c
Pooja S <poojarathod08@gmail.com>
2022-05-20 20:36:43 -0700

    First commit for AddNumbers program

---
+++ GitDemo/src/test/java/sampleProject/AddNumbers.java
@@ -0,0 +1,13 @@
+package sampleProject;
+
+public class AddNumbers {
+
+	public static void main(String[] args) {
+		
+		int i=3;
+		int j=5;
+		int sum = i+j;
+		System.out.println("Sum=" + sum);
+	}
+
+}
